b62db2fee8fb1daa63c7bac5498cf4b27fa445f8
Your Name <you@example.com>
2022-02-26 14:29:04 -0300

    Versão Com Valores das Cotaçoes e Layout pronto. | V - 1.0

--- script.js
+++ script.js
@@ -7,8 +7,40 @@ fetch(' https://economia.awesomeapi.com.br/last/USD-BRL').then((response) => res
 
     try {
         console.log(valorDolar)
-        dolar.innerHTML = valorDolar.slice(1, 7, 6, 5, 4) + ' R$'
+        dolar.innerHTML = valorDolar.slice(1, 5, 6, 7) + ' R$'
     } catch (error) {
         console.log('Deu Erro')
+    }
+})
+
+
+fetch(' https://economia.awesomeapi.com.br/last/EUR-BRL').then((response) => response.json()).then(data => {
+    const dolar = document.querySelector('.euro')
+
+    const option = JSON.stringify(data['EURBRL']['bid'])
+
+    const valorEuro = option.toString()
+
+    try {
+        console.log(valorEuro)
+        dolar.innerHTML = valorEuro.slice(1, 5, 6, 7) + ' R$'
+    } catch (error) {
+        console.log('Deu Erro Aqui')
+    }
+})
+
+
+fetch(' https://economia.awesomeapi.com.br/last/BTC-BRL').then((response) => response.json()).then(data => {
+    const dolar = document.querySelector('.bitcoin')
+
+    const option = JSON.stringify(data['BTCBRL']['bid'])
+
+    const valorBit = option.toString()
+
+    try {
+        console.log(valorBit)
+        dolar.innerHTML = valorBit.slice(1, 8) + ' R$'
+    } catch (error) {
+        console.error('Deu Erro Aqui')
     }
 })
